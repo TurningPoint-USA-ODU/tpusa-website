ac464a68ba902bf6d47aeff1d846e104bc10dcc4
Turning Point USA Old Dominion Chapter <tpusa@gmail.com>
2025-09-29 15:38:04 -0400

    Added base templates
    Added templates for contact, events, join, and leadership. Edited home
    to include static files

--- tpusa/main/urls.py
+++ tpusa/main/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("events/", views.events, name="events"),
+    path("contact/", views.contact, name="contact"),
+    path("join/", views.join, name="join"),
+    path("leadership/", views.leadership, name="leadership"),
 ]
--- tpusa/main/views.py
+++ tpusa/main/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
+
 
 # Create your views here.
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     return render(request, "main/index.html")
+
+
+def events(request: HttpRequest) -> HttpResponse:
+    return render(request, "main/events.html")
+
+
+def join(request: HttpRequest) -> HttpResponse:
+    return render(request, "main/join.html")
+
+
+def leadership(request: HttpRequest) -> HttpResponse:
+    return render(request, "main/leadership.html")
+
+
+def contact(request: HttpRequest) -> HttpResponse:
+    return render(request, "main/contact.html")
